5bf1be30fb7cd50506c401850fa5bd546d072ac6
Robert Rees <robert.rees@guardian.co.uk>
2015-12-16 19:52:56 +0000

    Creates a simple happy-path URL extractor

--- src/id_to_url/core.clj
+++ src/id_to_url/core.clj
@@ -1,10 +1,17 @@
 (ns id-to-url.core
     (:require
         [id-to-url.data :as data]
-        [org.httpkit.client :as http]))
+        [org.httpkit.client :as http]
+        [cheshire.core :as json]))
 
 (defn lookup-url [host id]
-    (http/get (str "http://" host "/internal-code/content/" id)))
+    {:id id :result (http/get (str "http://" host "/internal-code/content/" id))})
+
+(defn extract-url [result]
+    (let [
+        {id :id promise :result} result
+        json (json/parse-string (:body @promise) true)]
+        (get-in json [:response :content :webUrl])))
 
 (defn competition-ids [] data/competition-ids)
 
